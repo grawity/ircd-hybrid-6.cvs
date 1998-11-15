6211494f6c484f6fa46aec626d9633939d868c69
Diane Bruce <db@db.net>
1998-11-15 07:10:13 +0000

    changed MOTD_WAIT to PACE_WAIT fixed m_whois() I broke re-wrote x/q line code to use specialized link lists

--- src/channel.c
+++ src/channel.c
@@ -22,7 +22,7 @@
 static	char sccsid[] = "@(#)channel.c	2.58 2/18/94 (C) 1990 University of Oulu, Computing\
  Center and Jarkko Oikarinen";
 
-static char *rcs_version="$Id: channel.c,v 1.24 1998/11/06 22:35:20 db Exp $";
+static char *rcs_version="$Id: channel.c,v 1.25 1998/11/13 21:49:23 db Exp $";
 #endif
 
 #include "struct.h"
@@ -2790,7 +2790,7 @@ int	m_knock(aClient *cptr,
 
   if(!IsAnOper(sptr))
     {
-      if((last_used + MOTD_WAIT) > NOW)
+      if((last_used + PACE_WAIT) > NOW)
 	return 0;
       else
 	last_used = NOW;
