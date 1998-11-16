656a52f6de3fe37a97e783dabe938de65e785a49
Diane Bruce <db@db.net>
1998-11-16 16:37:13 +0000

    removed redundant comment in s_bsd.c fixed quarantined nick code

--- src/s_user.c
+++ src/s_user.c
@@ -25,7 +25,7 @@
 static  char sccsid[] = "@(#)s_user.c	2.68 07 Nov 1993 (C) 1988 University of Oulu, \
 Computing Center and Jarkko Oikarinen";
 
-static char *rcs_version="$Id: s_user.c,v 1.17 1998/11/13 21:49:26 db Exp $";
+static char *rcs_version="$Id: s_user.c,v 1.18 1998/11/15 07:10:13 db Exp $";
 
 #endif
 
@@ -1335,7 +1335,7 @@ int	m_nick(aClient *cptr,
     *s = '\0';
   strncpyzt(nick, parv[1], NICKLEN+1);
 
-  if(!IsAnOper(sptr) && find_special_conf(sptr->info,CONF_QUARANTINED_NICK)) 
+  if(!IsAnOper(sptr) && find_special_conf(nick,CONF_QUARANTINED_NICK)) 
     {
       sendto_realops("Quarantined nick [%s], dumping user %s",
 		     nick,get_client_name(cptr, FALSE));
