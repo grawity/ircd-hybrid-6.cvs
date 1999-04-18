c614c482dd65b3fee72681a0ed4f9f34557874bc
Diane Bruce <db@db.net>
1999-04-18 01:47:28 +0000

    * if client is being spoofed in I line, don't show real IP to non opers in trace or stats L * consistently use 127.0.0.1 for spoofed/opers ip's

--- src/s_conf.c
+++ src/s_conf.c
@@ -22,7 +22,7 @@
 static  char sccsid[] = "@(#)s_conf.c	2.56 02 Apr 1994 (C) 1988 University of Oulu, \
 Computing Center and Jarkko Oikarinen";
 
-static char *rcs_version = "$Id: s_conf.c,v 1.46 1999/03/27 06:20:23 db Exp $";
+static char *rcs_version = "$Id: s_conf.c,v 1.47 1999/03/27 11:14:23 db Exp $";
 #endif
 
 #include "struct.h"
@@ -245,6 +245,8 @@ int	attach_Iline(aClient *cptr,
 			     cptr->name,host,cptr->hostip,me.name);
 	      strncpyzt(cptr->sockhost,"oper.",sizeof(cptr->sockhost));
 	      strcat(cptr->sockhost,me.name);
+	      SetIPSpoof(cptr);
+	      SetIPHidden(cptr);
 	    }
 	  else
 	    strncpyzt(cptr->sockhost,host,sizeof(cptr->sockhost));
