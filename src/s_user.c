bd53c976766924f746c58e87864bb15fdf61bea6
Jon Lusky <lusky@blown.net>
1999-05-09 01:00:31 +0000

    * changed ANTI_DRONE_FLOOD and JUPE_CHANNEL to undeffed by default * servers and opers can no longer trip the drone detection:   if(MyConnect(acptr) && IsClient(sptr) && !IsAnOper(sptr) && drone_time)

--- src/s_user.c
+++ src/s_user.c
@@ -30,7 +30,7 @@
 static  char sccsid[] = "@(#)s_user.c	2.68 07 Nov 1993 (C) 1988 University of Oulu, \
 Computing Center and Jarkko Oikarinen";
 
-static char *rcs_version="$Id: s_user.c,v 1.70 1999/05/07 11:14:00 db Exp $";
+static char *rcs_version="$Id: s_user.c,v 1.71 1999/05/08 20:40:50 lusky Exp $";
 
 #endif
 
@@ -2060,7 +2060,7 @@ static	int	m_message(aClient *cptr,
 	    return 0;
 #endif
 #ifdef ANTI_DRONE_FLOOD
-      if(MyConnect(acptr) && drone_time)
+      if(MyConnect(acptr) && IsClient(sptr) && !IsAnOper(sptr) && drone_time)
 	{
 	  if((acptr->first_received_message_time+drone_time) < NOW)
 	    {
