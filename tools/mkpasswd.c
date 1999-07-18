bf3e3a3feaa3fc67687ba9e71606bb6952fc0218
Diane Bruce <db@db.net>
1999-07-18 02:43:07 +0000

    - removed last vestiges of references to sys.h in tools dir - gave up. its now ircd-hybrid-6-current for a while, rely on serno.h for now

--- tools/mkpasswd.c
+++ tools/mkpasswd.c
@@ -3,9 +3,8 @@
  * You can use this code as long as my name stays with it.
  *
  *
- * $Id: mkpasswd.c,v 1.1.1.1 1998/09/17 14:25:05 db Exp $
+ * $Id: mkpasswd.c,v 1.2 1999/07/15 08:47:47 tomh Exp $
  */
-#include "sys.h"
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
