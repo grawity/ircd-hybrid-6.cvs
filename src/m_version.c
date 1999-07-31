3003c18504da3c14f26e500a939ef76ed3c0c75b
Diane Bruce <db@db.net>
1999-07-31 04:05:52 +0000

    - moved m_message/m_notice/m_private into m_message.c - moved more stuff from struct.h to m_message.c - rebuilt .depend

--- src/m_version.c
+++ src/m_version.c
@@ -1,3 +1,5 @@
+
+
 /************************************************************************
  *   IRC - Internet Relay Chat, src/m_version.c
  *   Copyright (C) 1990 Jarkko Oikarinen and
@@ -20,7 +22,7 @@
  *   along with this program; if not, write to the Free Software
  *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
- *   $Id: m_version.c,v 1.2 1999/07/26 05:34:44 tomh Exp $
+ *   $Id: m_version.c,v 1.3 1999/07/28 07:49:37 tomh Exp $
  */
 #include "m_commands.h"
 #include "client.h"
