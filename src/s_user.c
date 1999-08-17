04951090b9c187f3d8f1d91749a91c0bec8b3dc5
Jon Lusky <lusky@blown.net>
1999-08-17 05:44:57 +0000

    removed redundant "New highest connections" server notice fixed SYSLOG_USERS bug (th) discard broken DNS replies (th) fixed IP mismatch check (th) revved patchlevel to b98

--- src/s_user.c
+++ src/s_user.c
@@ -20,7 +20,7 @@
  *   along with this program; if not, write to the Free Software
  *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
- *  $Id: s_user.c,v 1.209 1999/08/06 03:34:18 lusky Exp $
+ *  $Id: s_user.c,v 1.210 1999/08/12 03:02:21 lusky Exp $
  */
 #include "s_user.h"
 #include "channel.h"
@@ -350,10 +350,6 @@ int show_lusers(struct Client *cptr, struct Client *sptr,
   if ((m_client + m_server) > MaxConnectionCount)
     {
       MaxConnectionCount = m_client + m_server;
-      if (MaxConnectionCount % 10 == 0)
-        sendto_ops(
-                   "New highest connections: %d (%d clients)",
-                   MaxConnectionCount, MaxClientCount);
     }
 
   return 0;
