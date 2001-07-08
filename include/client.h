75073f00538bfc0951bd64ca76e80be65aa7871b
Andrew Miller <a1kmm@mware.virtualave.net>
2001-07-08 09:56:46 +0000

    - The first phase of adding KSERVER.

--- include/client.h
+++ include/client.h
@@ -17,7 +17,7 @@
  *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
  *
- * $Id: client.h,v 1.49 2001/06/25 02:29:02 greg Exp $
+ * $Id: client.h,v 1.50 2001/07/04 12:02:41 jdc Exp $
  */
 #ifndef INCLUDED_client_h
 #define INCLUDED_client_h
@@ -214,8 +214,10 @@ struct Client
 #ifdef ZIP_LINKS
   struct Zdata*     zip;        /* zip data */
 #endif
-#ifdef CRYPT_LINKS
+#if defined(CRYPT_LINKS) || defined(USE_KSERVER)
   struct CryptData *crypt;      /* Encrypted link info */
+#endif
+#ifdef CRYPT_LINKS
   struct CipherDef *cipher;     /* Supported cipher */
 #endif
   short             lastsq;     /* # of 2k blocks when sendqueued called last*/
