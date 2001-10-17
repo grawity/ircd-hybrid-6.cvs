b66d89a5dc58a6af7570f3bddd0ba6f9876cb10f
Diane Bruce <db@db.net>
2001-10-17 15:13:31 +0000

    - removed PACE_WALLOPS, this affects config.h m_info.h m_info.c   m_wallops.c m_operwall.c
    - cleaned up formatting in res.c so I could follow the logic, still
      havent' found the bug
    
    - cleaned up formatting in dbuf.c

--- src/m_wallops.c
+++ src/m_wallops.c
@@ -20,7 +20,7 @@
  *   along with this program; if not, write to the Free Software
  *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
- *   $Id: m_wallops.c,v 1.1 1999/07/29 07:11:49 tomh Exp $
+ *   $Id: m_wallops.c,v 1.2 1999/08/10 02:46:25 lusky Exp $
  */
 #include "m_commands.h"
 #include "client.h"
@@ -120,20 +120,6 @@ int m_wallops(struct Client *cptr, struct Client *sptr, int parc, char *parv[])
 
   if(!IsServer(sptr))   /* If source of message is not a server, i.e. oper */
     {
-
-#ifdef PACE_WALLOPS
-      if( MyClient(sptr) )
-        {
-          if( (LastUsedWallops + WALLOPS_WAIT) > CurrentTime )
-            { 
-          	sendto_one(sptr, ":%s NOTICE %s :Oh, one of those annoying opers who doesn't know how to use a channel",
-                     me.name,parv[0]);
-          	return 0;
-            }
-          LastUsedWallops = CurrentTime;
-        }
-#endif
-
       send_operwall(sptr, "WALLOPS", message);
       sendto_serv_butone( IsServer(cptr) ? cptr : NULL,
                           ":%s WALLOPS :%s", parv[0], message);
